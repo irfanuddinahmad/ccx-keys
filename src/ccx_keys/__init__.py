610eafbde539ee0dbc9dcb39c612c01cf2ef1e27
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2026-09-09 14:21:00 +0500

    fix: remove unnecessary try/except around __version__ (not present pre-migration)

--- src/ccx_keys/__init__.py
+++ src/ccx_keys/__init__.py
@@ -1,8 +1,5 @@
 """init"""
 
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import version
 
-try:
-    __version__ = version("edx-ccx-keys")
-except PackageNotFoundError:  # pragma: no cover
-    pass
+__version__ = version("edx-ccx-keys")
